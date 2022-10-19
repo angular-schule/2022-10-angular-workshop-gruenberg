78673a66ae70d8d51896ee28b7a26b15ab716d75
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-10-19 12:16:01 +0200

    simple books

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+
+  books: Book[] = [{
+    isbn: '000',
+    title: 'Angular',
+    description: 'Tolles Buch',
+    rating: 5
+  }, {
+    isbn: '111',
+    title: 'AngularJS',
+    description: 'Altes Buch',
+    rating: 3
+  }, {
+    isbn: '222',
+    title: 'jQuery',
+    description: 'Super altes Buch',
+    rating: 1
+  }];
+
+
+}
