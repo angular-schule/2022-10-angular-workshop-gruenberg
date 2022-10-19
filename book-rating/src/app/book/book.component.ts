131fa9cf69ceba3809833ea5c1bff4f6169e54fe
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-10-19 12:49:25 +0200

    *ngFor

---
+++ book-rating/src/app/book/book.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss']
+})
+export class BookComponent {
+
+  @Input()
+  book?: Book;
+}
