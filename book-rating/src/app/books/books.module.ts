dd97af4167831d8f47524e6b6b5f49e85ba4a60a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-10-20 10:10:58 +0200

    refactoring to modules

---
+++ book-rating/src/app/books/books.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BooksRoutingModule } from './books-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookComponent } from './book/book.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    BookComponent,
+    // und weitere wie RatingComponent, RatingPipe
+  ],
+  exports: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    BooksRoutingModule
+  ]
+})
+export class BooksModule { }
